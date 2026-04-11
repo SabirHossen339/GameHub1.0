eb6b3244b6dbeae3d08a473cc409cf9b9a97f27a
SabirHossen339 <sabirshimanto01729@gmail.com>
2026-04-12 00:21:12 +0600

    Working on PopularGames section

--- src/Components/PopularGames.jsx
+++ src/Components/PopularGames.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 
 
-const PopularGames = () => {
+const PopularGames = ({games}) => {
+    const navigate = useNavigate();
+
+    const selectGames = Array.isArray(games) ? games : [];
+
+    // top games
+    const popularGames = [...selectGames]
+    .sort((a,b)=> parseFloat(b.ratings) - parseFloat(a.ratings))
+    .slice(0,6);
    
     return (
-        <div className="mt-10 p-5">
-            <h1 className="text-5xl font-bold">Popular <span className="text-blue-400">Games</span></h1>
-            <p className="mt-2 text-gray-400">Curated picks from the indie community</p>
-        </div>
+       <section className="max-w-7xl px-5 mt-10">
+        {/* Title */}
+<div>
+    <h1 className="text-3xl lg:text-5xl md:text-5xl font-bold">Popular <span className="text-blue-400">Games</span></h1>
+            <p className="text-xl mt-2 text-gray-300 font-semibold">Curated picks from the indie community</p>
+</div>
+
+{/* GridLayout and card */}
+<div className="">
+    
+</div>
+
+       </section>
         
     );
 };
