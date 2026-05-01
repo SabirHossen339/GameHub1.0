db96209097f426e5fd69e01d6aad000306bf5f90
SabirHossen339 <sabirshimanto01729@gmail.com>
2026-05-01 21:41:21 +0600

    Working on All Games Section

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -6,10 +6,16 @@ const categoryPromise= fetch("/category.json").then((res)=>res.json());
 const Categories = () => {
     const categories = use(categoryPromise);
     return (
-        <div>
+        <div className="">
             <div className="grid grid-cols-2 md:grid-cols-6 lg:grid-cols-6 items-center gap-4">
                 {
-                categories.map(category=><NavLink key={category.id} className={"btn text-xl bg-linear-to-r from-blue-500 to-cyan-400 text-white hover:from-blue-600 hover:to-cyan-500 rounded-lg border-0 font-bold transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105"}
+                categories.map(category=><NavLink key={category.id} className={({ isActive }) =>
+            `btn text-xl rounded-lg border-0 font-semibold transition duration-300 ease-in-out 
+            ${isActive 
+              ? "bg-blue-500 text-white scale-105" 
+              : "bg-linear-to-r from-blue-500 to-cyan-400 text-white hover:from-blue-600 hover:to-cyan-500 hover:-translate-y-1 hover:scale-105"
+            }`
+          }
                 to={`/category/${category.id}`}
                 >{category.name}</NavLink>)
             }
--- src/Pages/AllGames.jsx
+++ src/Pages/AllGames.jsx
@@ -1,18 +1,28 @@
 import React, { Suspense } from 'react';
 import Navbar from '../Components/Navbar';
 import Categories from '../Components/Categories';
+import { useParams } from 'react-router';
+import Footer from '../Components/Footer';
 
 const AllGames = () => {
+    const {id}= useParams();
+    console.log(id);
     return (
         <div>
             <Navbar></Navbar>
-            <div className="mt-5 px-10">
-                <div className="">
+            {/* Category buttons */}
+            <div className="mt-5 px-10 ">
+                <div className="flex items-center justify-center gap-2">
                     <Suspense fallback={<span className="loading loading-spinner text-center text-info"></span>}>
                     <Categories></Categories>
                 </Suspense>
                 </div>
+                {/* Games Section */}
+                <div className="mt-5">
+                    <h1 className="text-center text-2xl font-bold">Games Category -{id}</h1>
+                </div>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
